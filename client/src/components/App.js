8933cfd079e1b068f6230c886bbb44a5cd0133b2
Matheus Souza Silva <matheusny2006@gmail.com>
2019-06-20 21:54:44 -0300

    App route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import AppBar from "./AppBar";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamView from "./streams/StreamView";
+import StreamEdit from "./streams/StreamEdit";
 
 const App = () => {
-    return <div>App!</div>
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <div>
+          <AppBar />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamView} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/components/streams/StreamView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StreamView = () => {
+    return (
+        <div>
+            StreamView!
+        </div>
+    )
+};
+
+export default StreamView;
